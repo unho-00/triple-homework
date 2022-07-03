c6c98462a020b7d73de9a089bf768d645976f872
unho-00 <djsgh94@naver.com>
2022-07-03 16:20:47 +0900

    setting : icon 이미지 추가

---
+++ src/constant/image.ts
@@ -0,0 +1,9 @@
+import triple from '../assets/img/triple2x.png'
+import google from '../assets/img/play-store2x.png'
+import apple from '../assets/img/badge-apple4x.png'
+
+export const image = {
+  triple,
+  google,
+  apple,
+}
